8203bef79f4d3c61cebfbe336f312b1b7a6b4588
farman ansari <farmanansari35728@gmail.com>
2024-11-01 07:10:41 -0700

    Add files via upload

---
+++ index.js
@@ -0,0 +1,92 @@
+
+// Que No. 1) - Add two Numbers........................................
+// function AddTwoNumbers(){
+//     let A = 5;
+//     let B = 2;
+//     let Sum = A + B;
+//     console.log(Sum);
+// }
+// AddTwoNumbers();
+
+
+//  Que No. 2) - Conditions Obeyed?.......................................
+// function Is_Valid(){
+//     let A = 5;
+//     let B = 3;
+//     if(A < 10 && A > B){
+//         console.log("true");
+//     }else{
+//         console.log("false");
+//     }
+// }
+// Is_Valid();
+
+
+//  Que No. 3) - Checking Conditions..........................................
+// function Check(){
+//     let A = 12;
+//     let B = 20;
+//     if(A % 10 == 0 || B % 10 == 0){
+//         console.log("true");
+//     }else{
+//         console.log("false");
+//     }
+// }
+// Check();
+
+
+//  Que No. 4) - first digit of 4 digit numbers.................................
+// function First_Digit(){
+//     let N = "4567";
+//     console.log(N[0]);
+// }
+// First_Digit();
+
+
+//  Que No. 5) - last digit of 4 digit Number....................................
+// function Last_Digit(){
+//     let N = "4567";
+//     console.log(N[3]);
+// }
+// Last_Digit();
+
+
+//  Que No. 6) - Remainder........................................................
+// function Find_the_remainder(){
+//     let A = 2;
+//     let B = 9;
+//     let Rem = B % A;
+//     console.log(Rem);
+// }
+// Find_the_remainder();
+
+
+//  Que No. 7)  Multiply two Numbers..............................................
+// function Multiply_two_number(){
+//     let A = 2;
+//     let B = 5;
+//     let Mul = A * B;
+//     console.log(Mul);
+// }
+// Multiply_two_number();
+
+
+//  Que No. 8)  Marks Calculator.................................................
+// function Sum(){
+//     let Maths = 50;                // total marks..170
+//     let English = 20;
+//     let JavaScript = 100;
+//     let Sum = Maths + English + JavaScript;
+//     console.log(Sum);
+// }
+// Sum();
+
+// function Average(){           // this is a average..34
+//     let Maths = 50;
+//     let English = 20;
+//     let JavaScript = 100;
+//     let Sum = Maths + English + JavaScript;
+//     let Average = Sum/5;
+//     console.log(Average);
+// }
+// Average();
